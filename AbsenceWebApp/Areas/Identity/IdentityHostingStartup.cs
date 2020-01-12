ed5c8e7b5aed388316f70d17043573adfab32eb5
hala-jiri <hala.jiri@gmail.com>
2020-01-13 00:13:39 +0100

    NavBar style and menu, scaffolding generator pages into Identity area

---
+++ AbsenceWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AbsenceWebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AbsenceWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace AbsenceWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
