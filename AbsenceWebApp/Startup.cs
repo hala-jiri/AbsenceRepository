99317b5500d1f864206f5fa08fea4d2d93969fb8
hala-jiri <hala.jiri@gmail.com>
2020-01-12 23:27:23 +0100

    Turn off confirmation of account

--- AbsenceWebApp/Startup.cs
+++ AbsenceWebApp/Startup.cs
@@ -30,7 +30,8 @@ namespace AbsenceWebApp
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
