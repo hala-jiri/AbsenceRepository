762f13b9c834ea2f64ff53764e160d21239e90fd
hala-jiri <hala.jiri@gmail.com>
2020-01-15 19:22:33 +0100

    Users controller + login partial css

---
+++ AbsenceWebApp/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AbsenceWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbsenceWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.Name);
+            // if null, user is not log in yet.
+
+            // return All user except currently log in user
+            return View(await _db.ApplicationUser.Where(u=>u.Id!=claim.Value).ToListAsync());
+        }
+    }
+}
